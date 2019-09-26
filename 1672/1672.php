de6b9f0283307e9db1942e754743d64d76d949ea
tdc-students <cuongtk@tdc.edu.vn>
2019-09-26 14:32:44 +0700

    commit

--- 1672/1672.php
+++ 1672/1672.php
@@ -25,7 +25,6 @@
         <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/1672.css" rel="stylesheet" type="text/css"/>
-         <link href="<?php echo $url_path ?>/css/star-rating-svg.css" rel="stylesheet" type="text/css"/>
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <script  src="<?php echo $url_path ?>/js/jquery-1.9.1.min.js" ></script>
           <script src="<?php echo $url_path ?>/js/1100.js"></script>
